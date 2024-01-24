8b472d14656b0b4eeadc223a4e5d90092bb16417
OlgaKashkur <112951675+OlgaKashkur@users.noreply.github.com>
2024-01-24 12:16:07 +0300

    fix: 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -8,6 +8,7 @@ const filesFolder =path.join(__dirname,'files-copy');
 /*fs.rmdir(filesFolder, () => { 
     console.log("Start"); 
 });*/
+fs.mkdir(filesFolder,{recursive: true},() => console.log('Folder created'));
 fs.readdir(filesFolder, (err,files) => {
 	if (err) throw err;
 	files.forEach(file => {
@@ -21,7 +22,6 @@ fs.readdir(copyFolder, (err, files) => {
     if (err) 
       console.log(err); 
     else { 
-        fs.mkdir(filesFolder,{recursive: true},() => console.log('Folder created'));
         files.forEach(file => {
             fs.copyFile(path.join(__dirname,"files",file), path.join(__dirname,"files-copy",file),(err) => {
                 if (err) throw err;
